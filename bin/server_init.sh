618f3272ed7fa4e8bb1e9aebdc1d9a83b31971d3
azhao1981 <azhao.1981@gmail.com>
2016-04-11 11:08:54 +0800

    add server init

---
+++ bin/server_init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+# init a remote server ssh server 
+SSHPUB="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8ontrCs2Ysm2IRUaPBbcO2DCN5GfgN6/dqXFQkLEX0yS3dwZeki4tfWVGLWQow0afMnLN36O0CBK2Om/qgrzSMo8CLFz7Z7go+HqsmpEL4kzcWTu1qXDhFI/cCO3qhqEHpK1CWXUtOg0IBkhN2qDfrvKNh0MP7h1sWCGTBO+E6HqnmbUq1lAbUJJHPeMp0pQOnyGqu/lJHFJAP1WQX6SM3X1rUvvj7r+nQHSsDxXkMZINKMwCjYVwI7x/cNN03y//qqqK0vWucNWrwvC4KhaSmhZ6YDtKZj1QzEaP/iwfVu19nF+v1P7LY6JA1SMBWK76H+pCCyX07RziQVwBsNVl azhao.1981@gmail.com"
+
+ssh $1 "set -x \
+    && mkdir -p ~/.ssh \
+    && echo $SSHPUB > ~/.ssh/authorized_keys 
+"
